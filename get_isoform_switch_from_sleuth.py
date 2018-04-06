1ddb5501545c89cc8ae68770083dfc4b911536c2
tdayris <thibault.dayris@outlook.fr>
2018-04-06 10:38:47 +0200

    Revertion

--- get_isoform_switch_from_sleuth.py
+++ get_isoform_switch_from_sleuth.py
@@ -24,29 +24,11 @@ def filtr_switch(files):
     ]
 
 
-def filtr_de(files):
-    yield from [
-        (
-            f[f[stat] <= threashold]["ens_gene"],
-            p,
-            stat,
-            threashold
-        )
-        for f, p in [
-            (pd.read_csv(f, sep="\t", index_col=0, header=0), f)
-            for f in files
-        ]
-        for stat in ["qval", "pval"]
-        for threashold in [0.001, 0.005, 0.01, 0.05]
-    ]
-
-
 @begin.start
 @begin.logging
 @begin.tracebacks
 @begin.convert(diffexp=begin.utils.tobool)
-def main(*files: "Path to multiple Sleuth files",
-         diffexp: "Extract diff. exp. instead of switches"=False):
+def main(*files: "Path to multiple Sleuth files"):
     for r, p, s, t in (filtr_de(files) if diffexp else filtr_switch(files)):
         logging.debug("Working on %s, with %s: %s" % (p, s, str(t)))
         r.to_csv("%s_isoform_switch_%s_%s.tsv" % (p, s, str(t)), sep="\t")
