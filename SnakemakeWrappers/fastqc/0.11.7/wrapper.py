1754a27f886fa7e31ad1ba642aae1c9749ac1e9a
tdayris <thibault.dayris@outlook.fr>
2019-04-18 11:30:01 +0200

    [dev](fastqc-wrapper): Remove useless tests

--- SnakemakeWrappers/fastqc/0.11.7/wrapper.py
+++ SnakemakeWrappers/fastqc/0.11.7/wrapper.py
@@ -7,15 +7,9 @@ __copyright__ = "Fish_N_CHIP 2017"
 __email__ = ["thibault.dayris@gustaveroussy.fr"]
 __license__ = "GPLV3"
 
-import os.path as op
-# import snakemake
-
 from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-
-if not op.exists(snakemake.input[0]):
-    raise FileNotFoundError(f"Could not find: {snakemake.input[0]}")
 
 shell_cmd = (
     "fastqc "                                 # Tool
@@ -23,7 +17,7 @@ shell_cmd = (
     "--outdir {snakemake.params.tempdir} "    # Output directory
     "--threads {snakemake.threads} "          # Number of threads used
     "{snakemake.input[0]} "                   # Path to fastq file
-    "{log}"                                   # Logging.
+    "{log}"                                   # Logging
 )
 
 shell(shell_cmd)
