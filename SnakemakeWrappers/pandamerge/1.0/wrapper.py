94f46dc792d645e85b19b10e7d415f176730fa5d
tdayris <thibault.dayris@outlook.fr>
2019-04-24 13:48:43 +0200

    [dev](Pandas merge): tested.

--- SnakemakeWrappers/pandamerge/1.0/wrapper.py
+++ SnakemakeWrappers/pandamerge/1.0/wrapper.py
@@ -11,42 +11,38 @@ import os.path as op
 from snakemake.utils import makedirs
 
 makedirs(op.dirname(snakemake.input[0]))
+makedirs(op.dirname(snakemake.log[0]))
 
-merged = None
-for count in snakemake.output.keys():
-    for path, sample in snakemake.input.items():
-        print("Working on {} -> {}".format(sample, path))
-        data = pd.read_csv(
-            path,
-            sep="\t",
-            index_col=0,
-            header=0,
-            dtype={
-                0: str,
-                1: pd.np.float,
-                2: pd.np.float,
-                3: pd.np.float,
-                4: pd.np.float
-            },
-            na_values=""
-        )
-
-        data = data[[count]]
-        data.columns = sample
-        try:
-            merged = pd.merge(
-                merged,
-                data,
-                left_index=True,
-                right_index=True
+with open(snakemake.log[0], "w") as log:
+    for count in snakemake.output.keys():
+        merged = None
+        for sample, path in snakemake.input.items():
+            log.write("\nWorking on {} -> {}".format(sample, path))
+            data = pd.read_csv(
+                path,
+                sep="\t",
+                index_col=0,
+                header=0
             )
-        except AttributeError:
-            merged_frame = data
-        del data
 
-    merged.to_csv(
-        snakemake.output[count],
-        sep="\t",
-        index=True,
-        header=True
-    )
+            data = data[[count]]
+            data.columns = [sample]
+            try:
+                merged = pd.merge(
+                    merged,
+                    data,
+                    left_index=True,
+                    right_index=True
+                )
+            except TypeError:
+                merged = data
+            log.write(str(merged.head()))
+            log.write("\n")
+            del data
+
+        merged.to_csv(
+            snakemake.output[count],
+            sep="\t",
+            index=True,
+            header=True
+        )
