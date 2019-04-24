fbbdfea8d7fb71c1b78545996e340e91604ecb0a
tdayris <thibault.dayris@outlook.fr>
2019-04-24 14:51:23 +0200

    [doc](Panda Merge): Comments and docstrings updated

--- SnakemakeWrappers/pandamerge/1.0/wrapper.py
+++ SnakemakeWrappers/pandamerge/1.0/wrapper.py
@@ -1,4 +1,4 @@
-"""Snakemake wrapper for Salmon Index."""
+"""Snakemake wrapper for Pandas Merge"""
 
 __author__ = "Thibault Dayris"
 __copyright__ = "Copyright 2018, Thibault Dayris"
@@ -10,14 +10,18 @@ import os.path as op
 
 from snakemake.utils import makedirs
 
-makedirs(op.dirname(snakemake.input[0]))
 makedirs(op.dirname(snakemake.log[0]))
 
+# Activate stdout logging
 with open(snakemake.log[0], "w") as log:
+    # Each key in the output dict is a column name
     for count in snakemake.output.keys():
         merged = None
+        # Each key in the input dict is a sample name
         for sample, path in snakemake.input.items():
             log.write("\nWorking on {} -> {}".format(sample, path))
+
+            # Load dataset
             data = pd.read_csv(
                 path,
                 sep="\t",
@@ -25,8 +29,11 @@ with open(snakemake.log[0], "w") as log:
                 header=0
             )
 
+            # Reduce dataset
             data = data[[count]]
             data.columns = [sample]
+
+            # Merge datasets
             try:
                 merged = pd.merge(
                     merged,
@@ -36,10 +43,16 @@ with open(snakemake.log[0], "w") as log:
                 )
             except TypeError:
                 merged = data
+
             log.write(str(merged.head()))
             log.write("\n")
             del data
 
+        # Build output dir if needed
+        if not op.dirname(snakemake.output[count]).exists():
+            makedirs(op.dirname(snakemake.output[count]))
+
+        # Save result
         merged.to_csv(
             snakemake.output[count],
             sep="\t",
