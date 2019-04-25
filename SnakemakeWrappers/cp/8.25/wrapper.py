178b00bbb36512609d85e4cf85bfa9d8aefeac35
tdayris <thibault.dayris@outlook.fr>
2019-04-25 11:20:08 +0200

    [dev](cp): Doc + empty "extra" parameter protection

--- SnakemakeWrappers/cp/8.25/wrapper.py
+++ SnakemakeWrappers/cp/8.25/wrapper.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
 
+"""Snakemake wrapper for bash copy"""
+
 __author__ = "Thibault Dayris"
 __copyright__ = "Fish_n_CHIP 2019"
 __email__ = "thibault.dayris@gustaveroussy.fr"
@@ -17,14 +19,16 @@ from snakemake.shell import shell
 with open(snakemake.params.cold_storage, "r") as cold_list:
     cold_storage = yaml.load(cold_list)
 
+# Prepare logging
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+extra = snakemake.params.get("extra", "")
 
 shell_cmd = ""
 for f in snakemake.input:
     symlink = any(f.startswith(cs) for cs in cold_storage)
     shell_cmd += (
         "cp "                                        # Tool
-        "{snakemake.params.extra} "                  # Optionnal parameters
+        "{extra} "                                   # Optionnal parameters
         f"{'' if symlink else '--symbolic-link'} "   # Symlink if needed
         f"{os.path.realpath(f)} "                    # Path to input file
         f"{os.path.realpath(snakemake.output[0])} "  # Path to output file
