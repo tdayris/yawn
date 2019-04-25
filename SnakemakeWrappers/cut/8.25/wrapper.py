a0fa7d50facafbe5c5488410e846b6eee187ce7d
tdayris <thibault.dayris@outlook.fr>
2019-04-25 11:20:43 +0200

    [dev](cut): Initial commit

---
+++ SnakemakeWrappers/cut/8.25/wrapper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+
+"""Snakemake wrapper for bash cut"""
+
+__author__ = "Thibault Dayris"
+__copyright__ = "Fish_n_CHIP 2019"
+__email__ = "thibault.dayris@gustaveroussy.fr"
+__license__ = "GPLV3"
+
+import os
+
+from snakemake.shell import shell
+
+# Prepare logging
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+delimiter = snakemake.params.get("delimiter", "$'\t'")
+columns = snakemake.params.get("columns", "3")
+extra = snakemake.params.get("extra", "")
+
+shell(
+    "cut "                   # Tool
+    "-f {columns} "          # Selection of columns/fields
+    "-d {delimiter} "        # Selection of delimiter
+    "{extra} "               # Extra parameters
+    "{snakemake.input} "     # Path to input file
+    "> {snakemake.output} "  # Path to output file
+    "{log}"
+)
