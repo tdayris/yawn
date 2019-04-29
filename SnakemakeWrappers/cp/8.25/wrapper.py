81ab79f2d2954961bb9b97f3ac83da28e5cd149a
tdayris <thibault.dayris@outlook.fr>
2019-04-29 10:00:48 +0200

    [dev](cp): Now possible to take list as cold storage

--- SnakemakeWrappers/cp/8.25/wrapper.py
+++ SnakemakeWrappers/cp/8.25/wrapper.py
@@ -16,8 +16,11 @@ from snakemake.shell import shell
 # No node can access a cold storage
 # these files must be copied. However,
 # any file else where should be symlinked!
-with open(snakemake.params.cold_storage, "r") as cold_list:
-    cold_storage = yaml.load(cold_list)
+if isinstance(snakemake.params.cold_storage, list):
+    cold_storage = snakemake.params.cold_storage
+else:
+    with open(snakemake.params.cold_storage, "r") as cold_list:
+        cold_storage = yaml.load(cold_list)
 
 # Prepare logging
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
